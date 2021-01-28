a261f9b3589c5e243c1672f9fdc0d121b5d27cb9
darkside550 <imran.saad50@gmail.com>
2021-01-28 17:39:05 +0530

    added scroll component as parent to cardlist component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from  './SearchBox'
 import {robots} from './robots';
+import Scroll from './Scroll';
 
 import './App.css'
 
@@ -12,9 +13,16 @@ class App extends Component {
     constructor(){
         super()
         this.state={
-            robots: robots,
+            robots: [],
             searchField: ''
         }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}))
+        
     }
 
     onSearchChange = (event) => {
@@ -26,13 +34,20 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robots =>{
             return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
         })
-        return (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots}/>
-            </div>
-        )
+
+        if (this.state.robots.length ===0 ){
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <CardList robots={filteredRobots}/>
+                    </Scroll>
+                </div>
+            )
+        }
     }
 }
 
